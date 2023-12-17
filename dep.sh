6921acb0ef7ea7757fa019eb8098c362035943f0
ybyan <ybyan@localhost>
2023-12-17 02:48:39 -0700

    add: script files

---
+++ dep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if rustup is installed
+if command -v rustup &> /dev/null
+then
+    echo "Rustup is already installed."
+else
+    echo "Rustup is not installed. Installing now."
+
+    # Download and run the Rust installation script
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+    # Add cargo to the PATH
+    source $HOME/.cargo/env
+
+    echo "Rustup, Rust and Cargo have been installed."
+fi
