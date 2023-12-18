03cea8626c7bdf6113074fccb9f30364a7e310f8
Yibo Yan <yiboyan@usc.edu>
2023-12-18 10:44:46 -0700

    mod: file changes

--- src/agent/src/main.rs
+++ src/agent/src/main.rs
@@ -1,3 +1,49 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::str;
+
+fn start_process() -> Child {
+    let child = Command::new("gdbserver")
+        .arg("127.0.0.1:7879")
+        .spawn()
+        .expect("Failed to start process");
+
+    child
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    // Read data from the stream
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            // Convert bytes to string and process the message
+            let msg = str::from_utf8(&buffer).unwrap();
+            println!("Received: {}", msg);
+
+            // You can write back to the stream if needed
+            // stream.write_all(b"Message received\n").unwrap();
+        }
+        Err(e) => println!("Failed to read from connection: {}", e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Bind the listener to localhost on port 7878
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("Server listening on port 7878");
+
+    // Accept connections and process them
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // Handle the connection in a separate function
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Connection failed: {}", e);
+            }
+        }
+    }
 }
