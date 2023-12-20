20fc1888e1854e1e4bb0b8d59986aa6816f46520
Yibo Yan <yiboyan@usc.edu>
2023-12-20 09:13:24 -0700

    update

--- src/agent/src/main.rs
+++ src/agent/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{Read, Write, BufReader, BufRead};
 use std::time::Duration;
 use std::{str, thread};
 use std::process::{Command, Child};
+use std::sync::mpsc;
 
 fn start_process() -> Child {
     let child = Command::new("gdbserver")
@@ -14,58 +15,77 @@ fn start_process() -> Child {
     child
 }
 
-fn handle_client(stream: &mut TcpStream, conn_stream: &mut TcpStream) {
-    let mut buffer = [0; 200];
-
-    let read_buf = BufReader::new(stream);
-
-    for line in read_buf.lines() {
-        let line = line.expect("error");
-        println!("Received: {}", line);
-        // You can process the line here
-    }
-
+fn handle_client(stream: &mut TcpStream, conn_stream: &mut TcpStream, sender: mpsc::Sender<&str>) {
+    let mut buffer = [0; 40960];
 
     // let mut data_buf = vec![0u8; 0];
     // stream.read_to_end(&mut data_buf).expect("failed to read");
 
     // let msg = str::from_utf8(&data_buf).unwrap();
     // println!("AGENT Received: {}", msg);
+    // let mut msg = String::from("");
 
-    return;
+    loop {
+        // Read data from the stream
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                if size == 0 { break }
 
-    // Read data from the stream
-    match stream.read_exact(&mut buffer) {
-        Ok(_) => {
-            // Convert bytes to string and process the message
-            let msg = str::from_utf8(&buffer).unwrap();
-            println!("AGENT Received: {}", msg);
+                println!("Size is not zero. Keep reading.");
 
-            conn_stream.write_all(&buffer).expect("Failed to pass data to gdbserver.");
-            conn_stream.flush().expect("Failed to flush");
-            println!("Finished writing");
-            let mut gdbserver_buf = [0; 200];
-            match conn_stream.read_exact(&mut gdbserver_buf) {
-                Ok(_) => {
-                    let msg = str::from_utf8(&gdbserver_buf).unwrap();
-                    println!("gdbserver Returned: {}", msg);
-                    stream.write_all(&gdbserver_buf).expect("Failed to pass data to gdb.");
-                    stream.flush().expect("Failed to flush");
-                },
-                Err(e) => eprintln!("Failed to receive data: {}", e),
+                // Convert bytes to string and process the message
+
+                let msg = str::from_utf8(&buffer).unwrap();
+                if msg.starts_with("+") { continue }
+
+                // msg += curr_msg;
+
+                println!("AGENT Received: {}", msg);
+
+                conn_stream.write_all(&buffer).expect("Failed to write");
+                conn_stream.flush().expect("Failed to flush");
+
+                let mut conn_buf = [0u8; 40960];
+                match conn_stream.read(&mut conn_buf) {
+                    Ok(size) => {
+                        let msg = str::from_utf8(&conn_buf).unwrap();
+                        println!("gdbserver returns: {}", msg);
+                        stream.write_all(&conn_buf).unwrap();
+                        stream.flush().unwrap();
+                    },
+                    Err(e) => eprintln!("failed to read from gdbserver buffer {}", e),
+                }
+                // conn_stream.read(&mut conn_buf).expect("Failed");
+
+                // for byte in buffer {
+                //     // if byte == 0 { break }
+
+                //     print!("{} ", byte);
+                // }
+                // println!();
+
+                // for c in msg.chars() {
+                //     print!("{} ", c as u8);
+                // }
+                // println!();
+
+                // You can write back to the stream if needed
+                // stream.write_all(b"Message received\n").unwrap();
             }
-
-            // You can write back to the stream if needed
-            // stream.write_all(b"Message received\n").unwrap();
+            Err(e) => println!("Failed to read from connection: {}", e),
         }
-        Err(e) => println!("Failed to read from connection: {}", e),
     }
+    // println!("FINAL MSG: {msg}");
+    println!("Stream finished.")
+
 }
 
 fn main() {
     // let mut child = start_process();
     // // child.kill().expect("Failed to gdbserver");
     // thread::sleep(Duration::from_secs(3));
+    let (tx, rx) = mpsc::channel();
+    tx.send("hello");
 
     let mut conn_stream = TcpStream::connect("127.0.0.1:7879")
                                         .expect("Failed to establish connection to gdbserver");
@@ -81,7 +101,7 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 // Handle the connection in a separate function
-                handle_client(&mut stream, &mut conn_stream);
+                handle_client(&mut stream, &mut conn_stream, tx.clone());
             }
             Err(e) => {
                 println!("Connection failed: {}", e);
