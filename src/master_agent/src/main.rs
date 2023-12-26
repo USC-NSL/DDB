7f24de0704f25f77515a3dcb4feb14b46332f693
Yibo Yan <yiboyan@usc.edu>
2023-12-26 11:33:13 -0700

    mod: testing for the mi mode

--- src/master_agent/src/main.rs
+++ src/master_agent/src/main.rs
@@ -1,3 +1,29 @@
+// Master Agent itself should also support GDB/MI
+// so that vs code adapter can support our master agent.
+
+use std::process::{Child, Command, Stdio, Output};
+use std::io;
+
+fn start_process() -> Child {
+    let output = Command::new("gdb")
+        .arg("../bin/hello_world")
+        .arg("--interpreter=mi")
+        // .stdin(cfg)
+        // .stdin(Stdio::piped())
+        .spawn()
+        // .output()
+        .expect("Failed to start process");
+
+    // child
+    output
+}
+
 fn main() {
-    println!("Hello, world!");
+    // for now, just directly use MI mode
+
+    // let stdin = io::stdin().lock();
+
+    let mut output = start_process();
+    output.wait();
+    println!("Output: {output:?}");
 }
