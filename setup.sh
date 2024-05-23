2afcda357e9777de71cec4bdd025705f29bf9128
Yibo Yan <yiboyan@usc.edu>
2024-05-22 19:03:13 -0600

    fix: unnecessary sudo for gdbinit

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 
 # Init gdb experience
-sudo echo "handle SIGUSR1 SIGUSR2 nostop noprint" >> ~/.gdbinit
-sudo echo "set auto-load safe-path /" >> ~/.gdbinit
+echo "handle SIGUSR1 SIGUSR2 nostop noprint" >> ~/.gdbinit
+echo "set auto-load safe-path /" >> ~/.gdbinit
 
 # init submodules
 git submodule update --init --recursive
