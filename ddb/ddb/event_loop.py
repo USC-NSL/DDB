90087474c2c938f84cba02fcfb59da04e9792c4c
Yibo Yan <yiboyan@usc.edu>
2024-06-04 16:37:11 -0600

    wip: replicate the error

--- ddb/ddb/event_loop.py
+++ ddb/ddb/event_loop.py
@@ -1,15 +1,36 @@
 import asyncio
 import threading
+from typing import Optional
 
 class EventLoopThread(threading.Thread):
     def __init__(self):
         super().__init__(daemon=True)
         self.loop = asyncio.new_event_loop()
         self.futures = {}
+        self.runnning = False
 
     def run(self):
         asyncio.set_event_loop(self.loop)
+        self.runnning = True
         self.loop.run_forever()
 
     def get_loop(self):
         return self.loop
+
+class GlobalRunningLoop:
+    _instance: Optional["GlobalRunningLoop"] = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(GlobalRunningLoop, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self) -> None:
+        self._loop = EventLoopThread()
+        threading.Thread(target=self._loop.run, args=()).start()
+
+    # def start(self):
+
+    def get_loop(self):
+        return self._loop.get_loop()
+
