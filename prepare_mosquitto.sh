5edcae0d88aab7bf830a39bda818f01eea614747
Yibo Yan <yiboyan@usc.edu>
2024-06-05 17:19:56 -0600

    fix: install mosquitto from source to prevent errors

--- prepare_mosquitto.sh
+++ prepare_mosquitto.sh
@@ -2,7 +2,17 @@
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sudo apt-get install -y mosquitto mosquitto-clients
+TMP_FOLDER="/tmp/mosquitto"
+
+rm -rf $TMP_FOLDER
+mkdir -p $TMP_FOLDER
+pushd $TMP_FOLDER
+git clone https://github.com/eclipse/paho.mqtt.c.git
+cd paho.mqtt.c
+make -j$(nproc)
+sudo make install
+
+# sudo apt-get install -y mosquitto mosquitto-clients
 
 # sudo mv /etc/mosquitto/mosquitto.conf /etc/mosquitto/mosquitto.conf.bak
 # pushd $SOURCE_DIR
