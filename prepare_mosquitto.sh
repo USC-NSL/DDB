36acc7f575ebb055b397703ae259913126c0746d
Yibo Yan <yiboyan@usc.edu>
2024-09-05 15:03:34 -0600

    mod: fix mosquitto setup script

--- prepare_mosquitto.sh
+++ prepare_mosquitto.sh
@@ -5,9 +5,10 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TMP_FOLDER="/tmp/mosquitto"
 
 sudo apt-get update
+sudo apt-get install -y build-essential gcc make cmake cmake-gui cmake-curses-gui 
 sudo apt-add-repository -y ppa:mosquitto-dev/mosquitto-ppa
 sudo apt-get update
-sudo apt-get install -y build-essential gcc make cmake cmake-gui cmake-curses-gui mosquitto
+sudo apt-get install -y libc-ares-dev libssl-dev mosquitto # install mosquitto broker directly
 
 rm -rf $TMP_FOLDER
 mkdir -p $TMP_FOLDER
@@ -15,7 +16,8 @@ pushd $TMP_FOLDER
 git clone https://github.com/eclipse/paho.mqtt.c.git
 cd paho.mqtt.c
 make -j$(nproc)
-sudo make install
+sudo make uninstall # clean up first
+sudo make install # install mosquitto c lib
 
 
 # sudo mv /etc/mosquitto/mosquitto.conf /etc/mosquitto/mosquitto.conf.bak
