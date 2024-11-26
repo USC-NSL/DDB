2d878bd66742ee2380512baa4f2df829f5db9c6c
Yibo Yan <yiboyan@usc.edu>
2024-11-25 20:18:34 -0700

    nits

--- scripts/rpc_framework_setup.sh
+++ scripts/rpc_framework_setup.sh
@@ -27,19 +27,21 @@ if [ "$1" = "clean" ]; then
   exit 0
 fi
 
-echo building grpc
-rm -rf $fw_folder/grpc/include/cereal $fw_folder/grpc/include/ddb >/dev/null 2>&1
-cp -r ../connector/cereal ../connector/ddb $fw_folder/grpc/include/
-cd $fw_folder/grpc
-if ! ./setup.sh; then
-  echo "Building grpc failed"
-  exit 1
-fi
-cd ..
+# echo building grpc
+# rm -rf $fw_folder/grpc/include/cereal $fw_folder/grpc/include/ddb >/dev/null 2>&1
+# cp -r ../connector/cereal ../connector/ddb $fw_folder/grpc/include/
+# cd $fw_folder/grpc
+# if ! ./setup.sh; then
+#   echo "Building grpc failed"
+#   exit 1
+# fi
+# cd ..
 
 echo building Nu
-rm -rf $fw_folder/Nu/include/ddb >/dev/null 2>&1
-cp -r ../connector/ddb $fw_folder/Nu/include/
+if [ -d "$fw_folder/Nu/include/ddb" ]; then
+  rm -rf "$fw_folder/Nu/include/ddb"
+fi
+cp -r ../connector/ddb $fw_folder/Nu/inc/
 cd $fw_folder/Nu
-NODE_TYPE=c6526 ./build_all.sh
+NODE_TYPE=c6525 ./build_all.sh
 cd ../../
