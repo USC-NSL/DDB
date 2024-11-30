6928a06b5e24f3ccd882bb709fdfb000212afc01
Yibo Yan <yiboyan@usc.edu>
2024-11-30 13:54:29 +0000

    fix: mistakenly ignore .txt (CMake) files

--- scripts/rpc_framework_setup.sh
+++ scripts/rpc_framework_setup.sh
@@ -27,21 +27,21 @@ if [ "$1" = "clean" ]; then
   exit 0
 fi
 
-# echo building grpc
-# rm -rf $fw_folder/grpc/include/cereal $fw_folder/grpc/include/ddb >/dev/null 2>&1
-# cp -r ../connector/cereal ../connector/ddb $fw_folder/grpc/include/
-# cd $fw_folder/grpc
-# if ! ./setup.sh; then
-#   echo "Building grpc failed"
-#   exit 1
-# fi
-# cd ..
-
-echo building Nu
-if [ -d "$fw_folder/Nu/include/ddb" ]; then
-  rm -rf "$fw_folder/Nu/include/ddb"
+echo building grpc
+rm -rf $fw_folder/grpc/include/cereal $fw_folder/grpc/include/ddb >/dev/null 2>&1
+cp -r ../connector/cereal ../connector/ddb $fw_folder/grpc/include/
+cd $fw_folder/grpc
+if ! ./setup.sh; then
+  echo "Building grpc failed"
+  exit 1
 fi
-cp -r ../connector/ddb $fw_folder/Nu/inc/
-cd $fw_folder/Nu
-NODE_TYPE=c6525 ./build_all.sh
-cd ../../
+cd ..
+
+# echo building Nu
+# if [ -d "$fw_folder/Nu/include/ddb" ]; then
+#   rm -rf "$fw_folder/Nu/include/ddb"
+# fi
+# cp -r ../connector/ddb $fw_folder/Nu/inc/
+# cd $fw_folder/Nu
+# NODE_TYPE=c6525 ./build_all.sh
+# cd ../../
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-bash ./dep.sh
+# bash ./dep.sh
 
 # source config.sh
 
@@ -13,8 +13,11 @@ bash ./dep.sh
 echo "handle SIGUSR1 SIGUSR2 nostop noprint" >> ~/.gdbinit
 echo "set auto-load safe-path /" >> ~/.gdbinit
 
+echo "handle SIGUSR1 SIGUSR2 nostop noprint" >> ~/.config/gdb/gdbinit
+echo "set auto-load safe-path /" >> ~/.config/gdb/gdbinit
+
 # init submodules
-git submodule update --init --recursive
+# git submodule update --init --recursive
 
 # allow gdb attach via pid
 echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
