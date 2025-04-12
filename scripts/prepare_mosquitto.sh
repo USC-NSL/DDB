1b6a33d747da8ffa8263cc0128e927f6e6cf3646
Yibo Yan <yiboyan@usc.edu>
2025-04-11 20:26:21 -0600

    mod(scripts): skip mosquitto broker installation

--- scripts/prepare_mosquitto.sh
+++ scripts/prepare_mosquitto.sh
@@ -5,24 +5,33 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo apt-get update
 sudo apt-get install -y build-essential gcc autoconf libtool pkg-config make cmake cmake-gui cmake-curses-gui git
 
-sudo apt-add-repository -y ppa:mosquitto-dev/mosquitto-ppa
-sudo apt-get update
-sudo apt-get install -y libc-ares-dev libssl-dev mosquitto # install mosquitto broker directly
+# sudo apt-add-repository -y ppa:mosquitto-dev/mosquitto-ppa
+# sudo apt-get update
+# sudo apt-get install -y libc-ares-dev libssl-dev mosquitto # install mosquitto broker directly
 
-# disable auto-start mosquitto service
-sudo systemctl disable mosquitto.service
-sudo systemctl stop mosquitto.service
-# hack cleanup if any instance is running
-sudo pkill -9 mosquitto
+# # disable auto-start mosquitto service
+# sudo systemctl disable mosquitto.service
+# sudo systemctl stop mosquitto.service
+# # hack cleanup if any instance is running
+# sudo pkill -9 mosquitto
 
-TMP_FOLDER="/tmp/mosquitto"
+TMP_FOLDER="/opt/mqtt"
 
-rm -rf $TMP_FOLDER
-mkdir -p $TMP_FOLDER
-pushd $TMP_FOLDER
+sudo rm -rf $TMP_FOLDER
+sudo mkdir -p $TMP_FOLDER
+sudo chmod 777 $TMP_FOLDER
+
+check_status() {
+    if [ $? -ne 0 ]; then
+        echo "Error: Previous command failed. Exiting."
+        exit 1
+    fi
+}
+
+cd $TMP_FOLDER
+check_status
 git clone https://github.com/eclipse/paho.mqtt.c.git
 cd paho.mqtt.c
 make -j$(nproc)
 sudo make uninstall # clean up first
 sudo make install   # install mosquitto c lib
-popd
